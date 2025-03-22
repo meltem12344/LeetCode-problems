71e059c74ca4c9c20130a415b18669219bfb9801
Meltem Orhan <111740556+meltem12344@users.noreply.github.com>
2025-03-22 09:58:16 +0300

    .

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def isPalindrome(self, x):
         if x>0:
-            reverse = int(str(x)[::-1])
-            if reverse == x:
+            reverse = int(str(x)[::-1]) # get the reverse of the number
+            if reverse == x: # if reverse is equal return true
                 return True
             return False
         else:
