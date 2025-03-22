161a2cbadc8588de9477edab0f21d2d5a174e4a6
Meltem Orhan <111740556+meltem12344@users.noreply.github.com>
2025-03-22 09:55:48 +0300

    .

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
-        words = s.split()
-        lastWord = words[-1]
-        return len(lastWord)
+        words = s.split() # seperate the words with split()
+        lastWord = words[-1] #  get the last word from the list of words
+        return len(lastWord) # length of last word is returned
         
